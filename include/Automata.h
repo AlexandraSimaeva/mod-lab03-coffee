ca6b18775afe3cd10032da6607487ec51e5a5d2f
Alexandra Simaeva <138254668+AlexandraSimaeva@users.noreply.github.com>
2025-04-24 23:17:04 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>û
+class Automata
+{
+private:
+	enum STATES { OFF, WAIT, ACCEPT, COOK, CHECK };
+	float cash;
+	std::vector<std::string> menu;
+	std::vector<float> prices;
+	STATES state;
+	int drink_num;
+
+public:
+	Automata(std::vector<std::string> menu, std::vector<float> prices);
+	std::string on();
+	std::string off();
+	void coin(float money);
+	void etMenu();
+	std::string getState();
+	int choice(int drink);
+	int check();
+	void cancel();
+	void cook();
+	void finish();
+};
+ 

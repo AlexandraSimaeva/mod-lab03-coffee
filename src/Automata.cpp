2356fb10af8dec3362257e188232c4ef074300de
Alexandra Simaeva <138254668+AlexandraSimaeva@users.noreply.github.com>
2025-04-25 19:16:39 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -22,16 +22,16 @@ std::string Automata::on() {
 }
 std::string Automata::off() {
     if (state == WAIT) {
-	state = OFF;
-	return("Coffee machine is off\n");
+        state = OFF;
+        return("Coffee machine is off\n");
     } else {
-	return("Operation rejected, machine can't be off during making drink\n");
+        return("Operation rejected, machine can't be off during making drink\n");
     }
 }
 void Automata::etMenu() {
     std::cout << "MENU\n";
     for (int i = 0; i < menu.size(); i++) {
-	std::cout << menu[i] << " - " << prices[i] << "\n";
+        std::cout << menu[i] << " - " << prices[i] << "\n";
     } 
     std::cout << std::endl;
 }
@@ -41,33 +41,33 @@ void Automata::coin(float money) {
         state = ACCEPT;
         std::cout << "Got " << money << " dollars\n" << std::endl;
     } else if (state == ACCEPT) {
-	cash += money;
-	std::cout << "Got " << money << " dollars\n";
-	std::cout << "Current sum " << cash << " dollars\n" << std::endl;
+        cash += money;
+        std::cout << "Got " << money << " dollars\n";
+        std::cout << "Current sum " << cash << " dollars\n" << std::endl;
     } else {
-	std::cout << "Operation rejected, get away your money\n" << std::endl;
+        std::cout << "Operation rejected, get away your money\n" << std::endl;
     }
 }
 std::string Automata::getState() {
     switch (state) {
     case OFF:
-	return  "OFF";
-	break;
+        return  "OFF";
+        break;
     case WAIT:
-	return  "WAIT";
-	break;
+        return  "WAIT";
+        break;
     case ACCEPT:
-	return  "ACCEPT";
-	break;
+        return  "ACCEPT";
+        break;
     case CHECK:
-	return  "CHECK";
-	break;
+        return  "CHECK";
+        break;
     case COOK:
-	return  "COOK";
-	break;
+        return  "COOK";
+        break;
     default:
-	return  "UNKNOWN STATE";
-	break;
+        return  "UNKNOWN STATE";
+        break;
     }
 }
 int Automata::choice(int drink) {
@@ -75,12 +75,12 @@ int Automata::choice(int drink) {
         drink_num = drink;
         if (drink >= 0 && drink < menu.size()) {
             std::cout << "\nYour drink is " << menu[drink] 
-	    << "\nStart cooking?" << std::endl;
+            << "\nStart cooking?" << std::endl;
             state = CHECK;
             return 1;
         } else {
             std::cout << "\nWrong index of drink" << std::endl;
-	    return -1;
+            return -1;
         }
     } else {
         std::cout << "\nOperation rejected, You can't now choose drink\n";
@@ -90,16 +90,16 @@ int Automata::choice(int drink) {
 int Automata::check() {
     if (state == CHECK) {
         if (cash < prices[drink_num]) {
-	    state = ACCEPT;
-	    std::cout << "\nNot enough money" << std::endl;
-	    return -1;
+            state = ACCEPT;
+            std::cout << "\nNot enough money" << std::endl;
+            return -1;
         } else {
-	    state = COOK;
-	    return 1;
+            state = COOK;
+            return 1;
         }
     } else {
-	std::cout << "\nOperation rejected, pay for the drink" << std::endl;
-	return -2;
+        std::cout << "\nOperation rejected, pay for the drink" << std::endl;
+        return -2;
     }
 }
 void Automata::cook() {
@@ -122,15 +122,15 @@ void Automata::cancel() {
 }
 void Automata::finish() {
     if (state == COOK) {
-	std::cout << "\nYour drink is done\n";
-	cash -= prices[drink_num];
-	if (cash > 0) {
-	    std::cout << "\nGet back change" << cash << " dollars" << std::endl;
-	}
-	cash = 0;
-	drink_num = -1;
-	state = WAIT;
+        std::cout << "\nYour drink is done\n";
+        cash -= prices[drink_num];
+        if (cash > 0) {
+            std::cout << "\nGet back change" << cash << " dollars" << std::endl;
+        }
+        cash = 0;
+        drink_num = -1;
+        state = WAIT;
     } else {
-	std::cout << "\nCan't be finished" << std::endl;
+        std::cout << "\nCan't be finished" << std::endl;
     }
 }
